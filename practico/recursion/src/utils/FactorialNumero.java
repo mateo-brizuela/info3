e9567f6aeff971fb7a771a5c62b6b3e46f896484
Mateo Brizuela <mateobrizuela44@gmail.com>
2024-08-22 11:16:16 -0300

    rama main: commit 8
    cambios:
    termine el primer ejercicio de recursividad
    lo testee y al parecer funciona

--- practico/recursion/src/utils/FactorialNumero.java
+++ practico/recursion/src/utils/FactorialNumero.java
@@ -1,11 +1,26 @@
 package utils;
 
 public class FactorialNumero {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        int numero = 5;
+        int resultado = 0;
+        resultado = CFactorial(numero);
+        System.out.println("el resultado del factorial de "+ numero+
+        " es "+ resultado);
+
+        
+    }
+
+    public static int CFactorial(int numero){
+
+        if (numero == 1 || numero == 0) {
+            return 1;
+        }else{
+
+            return numero * CFactorial(numero - 1);
+        }
+    }
 
 
-
-}
-
-public static int CFactorial(int numero){
-    
 }
