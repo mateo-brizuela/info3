cb5aba807f3d7beb3765bc52cd5c9f554aaa618e
Daniel Olguin <danieldavidolguin@gmail.com>
2024-09-03 17:29:33 -0300

    2da correcion

--- practico/proyecto2/gestion-pizzeria/src/Ordenador.java
+++ practico/proyecto2/gestion-pizzeria/src/Ordenador.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 public abstract class Ordenador {
 
     public static void ordenarPorTiempo(ArrayList<Pedido> lista){
-        for(int i = 0; i < lista.size(); i++){ // recorre toda la lista de elementos
+        int i = 0, j = 0;
+        for(i = 0; i < lista.size(); i++){ // recorre toda la lista de elementos
             var temp = lista.get(i); // guarda el elemento que vamos a comparar
-            for(int j = i; j > 0 && lista.get(j-1).getTiempo() > temp.getTiempo(); j--){ // empieza desde uno antes y debe recorrer todos lo que tenga detrás
+            for(j = i; j > 0 && lista.get(j-1).getTiempo() > temp.getTiempo(); j--){ // empieza desde uno antes y debe recorrer todos lo que tenga detrás
                 lista.set(j,lista.get(j-1));
                 // lista.set(j-1,temp); // no hace falta, esto simplemente consume una op de mas
             }
+            lista.set(j,temp);
         }
     }
     
