9aa45c1bd35f5c61c9fa17b35a9f8d9567813cf4
Daniel Olguin <danieldavidolguin@gmail.com>
2024-09-03 17:14:51 -0300

    Correccion del metodo para ordenar por tiempo de prep

--- practico/proyecto2/gestion-pizzeria/src/Ordenador.java
+++ practico/proyecto2/gestion-pizzeria/src/Ordenador.java
@@ -4,10 +4,10 @@ public abstract class Ordenador {
 
     public static void ordenarPorTiempo(ArrayList<Pedido> lista){
         for(int i = 0; i < lista.size(); i++){ // recorre toda la lista de elementos
-            var temp = lista.get(i);
-            for(int j = i; j > 0 && lista.get(j-1).getTiempo() > lista.get(j).getTiempo(); j--){ // empieza desde uno antes y debe recorrer todos lo que tenga detrás
+            var temp = lista.get(i); // guarda el elemento que vamos a comparar
+            for(int j = i; j > 0 && lista.get(j-1).getTiempo() > temp.getTiempo(); j--){ // empieza desde uno antes y debe recorrer todos lo que tenga detrás
                 lista.set(j,lista.get(j-1));
-                lista.set(j-1,temp);
+                // lista.set(j-1,temp); // no hace falta, esto simplemente consume una op de mas
             }
         }
     }
