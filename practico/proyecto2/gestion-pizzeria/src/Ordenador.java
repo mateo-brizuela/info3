ec89fbc7b219a5b2b29cc5ee87d4d545964e082e
Daniel Olguin <danieldavidolguin@gmail.com>
2024-09-03 17:01:29 -0300

    Agregado metodo para ordenar por tiempo de preparacion

--- practico/proyecto2/gestion-pizzeria/src/Ordenador.java
+++ practico/proyecto2/gestion-pizzeria/src/Ordenador.java
@@ -1,3 +1,15 @@
-public class Ordenador {
+import java.util.ArrayList;
+
+public abstract class Ordenador {
+
+    public static void ordenarPorTiempo(ArrayList<Pedido> lista){
+        for(int i = 0; i < lista.size(); i++){ // recorre toda la lista de elementos
+            var temp = lista.get(i);
+            for(int j = i; j > 0 && lista.get(j-1).getTiempo() > lista.get(j).getTiempo(); j--){ // empieza desde uno antes y debe recorrer todos lo que tenga detrás
+                lista.set(j,lista.get(j-1));
+                lista.set(j-1,temp);
+            }
+        }
+    }
     
 }
